cdcde4ea2a30ffca9fbade5a43bb64a8de6516e3
laxman1129 <laxman1129@gmail.com>
2019-11-11 00:50:36 +0400

    added endpoint for getting recipe added new recipes
    need to remove datafile.pickle before starting application

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from flask_cors import CORS, cross_origin
 
-from elasticsearch import Elasticsearch
-
 import requests
 import json
 
@@ -13,17 +11,23 @@ import Recommender as rc
 app = Flask(__name__)
 api = Api(app)
 
-host = 'https://smartchef.eastus.cloudapp.azure.com'
-es = Elasticsearch(hosts=[host], use_ssl=True, verify_certs=False)
 
+def get_recommendation(recipe_keywords):
+    indexes = fs.get_closest_match(recipe_keywords, count=15)
+    print(indexes)
+    titles = fs.get_titles(indexes)
+    recommendations = rc.get_recommendations(titles[0])
+    recommendation_list = list(recommendations)
+    ids = fs.get_id(recommendation_list)
+    print(ids)
+    for id in ids:
+        # recipe_found = es.get(index='recipe-index', doc_type='recipe', id=id)
+        recipe_found = requests.get(
+            'https://smartchef.eastus.cloudapp.azure.com:9200/recipe-index/recipe/' + str(id),
+            verify=False)
+        # print(recipe_found.json()['_source'])
+        return recipe_found
 
-# Index — Database
-# Datatype — Type of the document
-# Id — Id of the document
-
-# Inserting a document:
-# Now let's store this document in Elasticsearch
-# res = es.index(index='megacorp',doc_type='employee',id=1,body=e1)
 
 class Test(Resource):
     @cross_origin()
@@ -48,33 +52,29 @@ class ContentCreator(Resource):
                     recipe_keywords = route + ' ' + name + ' ' + title + ' ' + ingredients
                     # pre-process the keywords
                     print(recipe_keywords)
-                    indexes = fs.get_closest_match(recipe_keywords, count=15)
-                    print(indexes)
-                    titles = fs.get_titles(indexes)
-                    recommendations = rc.get_recommendations(titles[0])
-                    recommendation_list = list(recommendations)
-                    ids = fs.get_id(recommendation_list)
-                    print(ids)
-                    for id in ids:
-                        # recipe_found = es.get(index='recipe-index', doc_type='recipe', id=id)
-                        recipe_found = requests.get(
-                            'https://smartchef.eastus.cloudapp.azure.com:9200/recipe-index/recipe/' + str(id),
-                            verify=False)
-                        # print(recipe_found.json()['_source'])
-                        recipe['recommentions'].append(recipe_found.json()['_source'])
+                    recipe_found = get_recommendation(recipe_keywords)
+                    recipe['recommentions'].append(recipe_found.json()['_source'])
 
-        print(email_request[0])
+        # print(email_request[0])
         # res = es.index(index='emailrequests-index', doc_type='emailrequests',
         #                body=json.dumps(emailRequest=email_request[0]))
         url = 'https://smartchef.eastus.cloudapp.azure.com:9200/emailrequests-index/emailrequests/'
-        print(url)
         headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
         response = requests.post(url, data=json.dumps(email_request[0]), headers=headers, verify=False)
         return 'successful'
 
 
+class Recommender(Resource):
+    @cross_origin()
+    def get(self, query):
+        print(query)
+        recipe_found = get_recommendation(query)
+        return jsonify(recipe_found.json()['_source'])
+
+
 api.add_resource(Test, '/')
 api.add_resource(ContentCreator, '/menu')
+api.add_resource(Recommender, '/get_recipe/<string:query>')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
--- util.py
+++ util.py
@@ -37,3 +37,33 @@
 #     jsonstr = jfile.read()
 #     data = json.loads(jsonstr)
 #     print(data)
+
+# import json
+# from numpy.random import seed
+# from numpy.random import randint
+# seed(43)
+# value = randint(5, 50)
+# print(value)
+#
+# all_new_recipes = open('data/all_meals_new.json', 'r')
+# d = json.loads(all_new_recipes.read())
+# j52981 = open('data/52981.json', 'r')
+# d52981 = json.loads(j52981.read())
+# d['recipes'].append(d52981)
+# j52982 = open('data/52982.json', 'r')
+# d52982 = json.loads(j52982.read())
+# d['recipes'].append(d52982)
+# j52983 = open('data/52983.json', 'r')
+# d52983 = json.loads(j52983.read())
+# d['recipes'].append(d52983)
+# j52984 = open('data/52984.json', 'r')
+# d52984 = json.loads(j52984.read())
+# d['recipes'].append(d52984)
+# # print(d['recipes'])
+#
+# # print(d['recipes'][0])
+# for i in d['recipes'][0]:
+#     i['budget'] =str(randint(5, 50))+'AED'
+#
+# f = open('data/new_recipe.json', 'w')
+# json.dump({'recipes': d['recipes']}, f)
